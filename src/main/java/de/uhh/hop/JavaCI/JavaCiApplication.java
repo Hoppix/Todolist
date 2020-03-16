6a10685650826355b0d74088892af86b437d2130
Hoppix <k.hopfmann@hotmail.de>
2020-03-16 12:05:15 +0100

    port change

--- src/main/java/de/uhh/hop/JavaCI/JavaCiApplication.java
+++ src/main/java/de/uhh/hop/JavaCI/JavaCiApplication.java
@@ -3,11 +3,17 @@ package de.uhh.hop.JavaCI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class JavaCiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaCiApplication.class, args);
+		//SpringApplication.run(JavaCiApplication.class, args);
+		 SpringApplication app = new SpringApplication(JavaCiApplication.class);
+         app.setDefaultProperties(Collections
+           .singletonMap("server.port", "8083"));
+         app.run(args);
 	}
 
 }
